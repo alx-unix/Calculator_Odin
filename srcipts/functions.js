3419015e86333017353d396f059b8815c34140dd
alex.linux <183350467+alx-unix@users.noreply.github.com>
2024-12-17 10:52:47 +0100

    Almost done, running tests to see if everythin works alright

--- srcipts/functions.js
+++ srcipts/functions.js
@@ -1,23 +1,42 @@
 function add(num1, num2){
-    return num1 + num2
+    number1 = Number(num1);
+    number2 = Number(num2);
+    return number1 + number2
 }
 
 function substract(num1, num2){
-    return num1 - num2
+    number1 = Number(num1);
+    number2 = Number(num2);
+    return number1 - number2
 }
 
 function multiply(num1, num2){
-    return num1 * num2
+    number1 = Number(num1);
+    number2 = Number(num2);
+    return number1 * number2
 }
 
 function divide(num1, num2){
-    return num1 / num2
+    number1 = Number(num1);
+    number2 = Number(num2);
+    if(number2 === 0){
+        alert("No dividing with zero you absolute animal")
+        return clearDisplay()
+    }
+    return number1 / number2
 }
 
 function power(num1, num2){
-    return num1**num2
+    number1 = Number(num1);
+    number2 = Number(num2);
+    return number1**number2
 }
 
 function operate(operator,num1,num2){
     return operator(num1,num2)
+}
+
+function clearDisplay(){
+    display2.textContent = 0;
+    display1.textContent = "";
 }
--- srcipts/main.js
+++ srcipts/main.js
@@ -1,24 +1,82 @@
 const deleteScreen = document.querySelector('.delete');
 const clear = document.querySelector('.clear');
-const display = document.querySelector(".screen-span");
-const buttons = document.querySelectorAll(".btn");
+const display2 = document.querySelector(".screen-div2");
+const display1 = document.querySelector(".screen-div1");
+const buttons = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".oper");
+const egal = document.querySelector(".equal")
 
-
+console.log(buttons)
+console.log(operations)
 
 
 
 
 for (let i=0; i<buttons.length; i++){
     buttons[i].addEventListener('click',()=>{
-        if (display.textContent == 0){
-            display.textContent = buttons[i].textContent
+        if (display2.textContent == 0){
+            display2.textContent = buttons[i].textContent
         }else{
-            display.textContent += buttons[i].textContent
+            display2.textContent += buttons[i].textContent
         }
-})
+    })
+}
+
+for (let i=0; i<operations.length; i++){
+    operations[i].addEventListener('click',()=>{
+        if (display2.textContent == 0){
+            alert('Nothing there mate')
+        }else if(!display1.textContent){
+            const num1 = display2.textContent
+            display1.textContent = num1 + operations[i].textContent
+            display2.textContent = ""
+            console.log('alright')
+        }else{
+            console.log('comes here')
+        }
+        egal.addEventListener('click',()=>{
+            console.log('alright2')
+            if (display1.textContent.includes('+')){
+                console.log('alright3')
+                const num2 = display2.textContent;
+                const num1 = display1.textContent;
+                const num1Filtered = num1.slice(0,-1);
+                const result = operate(add,num1Filtered, num2);
+                display1.textContent = "";
+                display2.textContent = result;
+            }else if(display1.textContent.includes('-')){
+                console.log('alright4')
+                const num2 = display2.textContent;
+                const num1 = display1.textContent;
+                const num1Filtered = num1.slice(0,-1);
+                const result = operate(substract,num1Filtered, num2);
+                display1.textContent = "";
+                display2.textContent = result;
+            }else if(display1.textContent.includes('*')){
+                console.log('alright5')
+                const num2 = display2.textContent;
+                const num1 = display1.textContent;
+                const num1Filtered = num1.slice(0,-1);
+                const result = operate(multiply,num1Filtered, num2);
+                display1.textContent = "";
+                display2.textContent = result;
+            }else if(display1.textContent.includes('/')){
+                console.log('alright5')
+                const num2 = display2.textContent;
+                const num1 = display1.textContent;
+                const num1Filtered = num1.slice(0,-1);
+                const result = operate(divide,num1Filtered, num2);
+                display1.textContent = "";
+                display2.textContent = result;
+            }
+                
+            })
+            
+    })
 }
 
 
+
 clear.addEventListener('click', ()=>{
-    display.textContent = 0
+    clearDisplay()
 })
